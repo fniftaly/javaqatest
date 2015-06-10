dd809ba9a5ea6a897ab5956b9f0e10ce91dd5599
Farad Niftaly <atbulag@yahoo.com>
2015-06-10 10:01:24 -0700

    commit processFiles.java

---
+++ NetBeansProjects/JavaQEtest/src/com/filesystem/ProcessFiles.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.filesystem;
+
+/**
+ *
+ * @author def
+ */
+public class ProcessFiles {
+    
+    private String filename;
+    
+    private String dir;
+    
+    private Create_dir_file _dirfile;
+    
+    public ProcessFiles(Create_dir_file dir){
+        this._dirfile = dir;
+        this.filename = "userobj.ser";
+    }
+    
+    public static void main(String[] args){
+        
+        ProcessFiles prf = new ProcessFiles(new CreateFileDir());
+        
+        /**
+         *   call all methods through _dirfile object;
+         * 
+         * 
+         */
+      
+    }
+    
+}
